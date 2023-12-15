4060a54ade5ed7265d8c6ae937aacfd59c874a59
Levente Hunyadi <levente.hunyadi@instructure.com>
2023-12-15 16:18:57 +0100

    Propagate field default value when transforming a Python class into an entity class

---
+++ pysqlsync/model/entity_types.py
@@ -0,0 +1,68 @@
+import dataclasses
+import sys
+from typing import Any, Optional
+
+from strong_typing.inspection import DataclassField, DataclassInstance, dataclass_fields
+
+from .key_types import PrimaryKey
+
+
+def key_value_fields(
+    cls: type[DataclassInstance], key: str
+) -> tuple[DataclassField, list[DataclassField]]:
+    """
+    Separates the key and the value fields in a data-class definition.
+
+    :param cls: The data-class type to transform.
+    :param key: The field to be come the primary key.
+    """
+
+    source_fields = dataclass_fields(cls)
+    key_field: Optional[DataclassField] = None
+    value_fields: list[DataclassField] = []
+    for source_field in source_fields:
+        if source_field.name == key:
+            key_field = source_field
+        else:
+            value_fields.append(source_field)
+
+    if key_field is None:
+        raise ValueError(f"key field not found: {key}")
+
+    return key_field, value_fields
+
+
+def make_entity(cls: type[DataclassInstance], key: str) -> type[DataclassInstance]:
+    """
+    Transforms a regular data-class type into an entity type.
+
+    :param cls: The data-class type to transform.
+    :param key: The field to be come the primary key.
+    """
+
+    key_field, value_fields = key_value_fields(cls, key)
+    key_type = key_field.type
+
+    primary_key_type = PrimaryKey[key_type]  # type: ignore
+    target_fields: list[tuple[str, Any, dataclasses.Field]] = [
+        (key_field.name, primary_key_type, dataclasses.field(default=key_field.default))
+    ]
+    for value_field in value_fields:
+        target_fields.append(
+            (
+                value_field.name,
+                value_field.type,
+                dataclasses.field(default=value_field.default),
+            )
+        )
+
+    class_name = cls.__name__
+    class_module = cls.__module__
+    class_doc = cls.__doc__
+
+    class_type = dataclasses.make_dataclass(
+        class_name, target_fields, namespace={"__module__": class_module}
+    )
+    class_type.__doc__ = class_doc
+    setattr(sys.modules[class_module], class_name, class_type)
+    return class_type
--- tests/test_types.py
+++ tests/test_types.py
@@ -1,8 +1,13 @@
 import enum
 import unittest
 from dataclasses import dataclass
-from typing import Annotated, Optional, TypeVar
+from typing import Annotated, ClassVar, Optional, TypeVar
 
+from pysqlsync.formation.inspection import (
+    dataclass_primary_key_name,
+    dataclass_primary_key_type,
+)
+from pysqlsync.model.entity_types import make_entity
 from pysqlsync.python_types import dataclass_to_code, enum_class_to_code
 
 
@@ -24,12 +29,13 @@ class Example:
     :param var_str: A string.
     """
 
-    static_var_int = 1
-    static_var_str = "string"
+    static_var_int: ClassVar[int] = 1
+    static_var_str: ClassVar[str] = "string"
 
     var_bool: bool
     var_int: TypeWrapper[int]
     var_str: str
+    var_def: int = 23
     var_opt: Optional[str] = None
 
 
@@ -60,6 +66,7 @@ class TestTypes(unittest.TestCase):
             "    var_bool: bool\n"
             "    var_int: Annotated[int, TypeTag()]\n"
             "    var_str: str\n"
+            "    var_def: int = 23\n"
             "    var_opt: Optional[str] = None\n",
         )
 
@@ -73,6 +80,28 @@ class TestTypes(unittest.TestCase):
             "    active = 'active'\n"
             "    inactive = 'inactive'\n"
             "    deleted = 'deleted'\n",
+        )
+
+    def test_entity(self) -> None:
+        cls = make_entity(Example, "var_str")
+        self.assertEqual(dataclass_primary_key_name(cls), "var_str")
+        self.assertEqual(dataclass_primary_key_type(cls), str)
+        self.assertMultiLineEqual(
+            dataclass_to_code(Example),
+            "@dataclasses.dataclass\n"
+            "class Example:\n"
+            '    """\n'
+            "    Documentation string.\n"
+            "\n"
+            "    :param var_int: An integer.\n"
+            "    :param var_str: A string.\n"
+            '    """\n'
+            "\n"
+            "    var_str: Annotated[str, PrimaryKey]\n"
+            "    var_bool: bool\n"
+            "    var_int: Annotated[int, TypeTag()]\n"
+            "    var_def: int = 23\n"
+            "    var_opt: Optional[str] = None\n",
         )
 
 
