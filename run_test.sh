377c21f90ccb47e53b5dc54c11eee9b1f16233e5
Levente Hunyadi <levente.hunyadi@instructure.com>
2024-01-10 14:25:17 +0100

    Optimize workflow for automated integration tests

--- run_test.sh
+++ run_test.sh
@@ -16,7 +16,10 @@ TEST_INTEGRATION=1 TEST_POSTGRESQL=1 TEST_MYSQL=1 python3 -m unittest discover
 CONTAINER=oracle-db
 docker ps -q --filter "name=$CONTAINER" | xargs -r docker stop
 docker ps -aq --filter "name=$CONTAINER" | xargs -r docker rm
-docker run -d --name $CONTAINER -e "ORACLE_PWD=<YourStrong@Passw0rd>" -p 1521:1521 container-registry.oracle.com/database/free:latest
+docker run -d \
+    -e "ORACLE_PWD=<YourStrong@Passw0rd>" \
+    -p 1521:1521 \
+    --name $CONTAINER container-registry.oracle.com/database/free:latest
 sleep 30
 TEST_INTEGRATION=1 TEST_ORACLE=1 python3 -m unittest discover
 docker stop $CONTAINER && docker rm $CONTAINER
@@ -25,7 +28,10 @@ docker stop $CONTAINER && docker rm $CONTAINER
 CONTAINER=sql1
 docker ps -q --filter "name=$CONTAINER" | xargs -r docker stop
 docker ps -aq --filter "name=$CONTAINER" | xargs -r docker rm
-docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=<YourStrong@Passw0rd>" -p 1433:1433 --name $CONTAINER --hostname sql1 -d mcr.microsoft.com/mssql/server:2022-latest
+docker run -d \
+    -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=<YourStrong@Passw0rd>" \
+    --hostname sql1 -p 1433:1433 \
+    --name $CONTAINER mcr.microsoft.com/mssql/server:2022-latest
 sleep 30
 TEST_INTEGRATION=1 TEST_MSSQL=1 python3 -m unittest discover
 docker stop $CONTAINER && docker rm $CONTAINER
