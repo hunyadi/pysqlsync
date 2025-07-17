1e2883dc07164ef0d40343c3f6384faa5067fa48
Levente Hunyadi <hunyadi@gmail.com>
2025-07-17 19:50:43 +0200

    Switch from `setup.cfg` to `pyproject.toml`

--- package.sh
+++ package.sh
@@ -1,13 +1,18 @@
 set -e
 
-PYTHON=python3
+PYTHON_EXECUTABLE=${PYTHON:-python3}
+
+if [ -d dist ]; then rm -rf dist; fi
+for dir in *.egg-info; do
+    if [ -d "$dir" ]; then rm -rf "$dir"; fi
+done
+
+./check.sh
+$PYTHON_EXECUTABLE -m build --sdist --wheel
 
 # Verify if the package builds successfully for all Python target versions
-if [ -d dist ]; then rm -rf dist; fi
-if [ -d pysqlsync.egg-info ]; then rm -rf pysqlsync.egg-info; fi
-./check.sh
-$PYTHON -m build
 docker build --build-arg PYTHON_VERSION=3.9 .
 docker build --build-arg PYTHON_VERSION=3.10 .
 docker build --build-arg PYTHON_VERSION=3.11 .
 docker build --build-arg PYTHON_VERSION=3.12 .
+docker build --build-arg PYTHON_VERSION=3.13 .
